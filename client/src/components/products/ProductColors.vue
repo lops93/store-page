d85be590342d7991f984d0d061239c6894881ea1
Viviam <vivian.lopesg@gmail.com>
2024-05-10 00:43:38 +0100

    added color picker function

--- client/src/components/products/ProductColors.vue
+++ client/src/components/products/ProductColors.vue
@@ -6,7 +6,7 @@
       <RadioGroupOption as="template" v-for="color in colors" :key="color.name" :value="color" v-slot="{ active, checked }">
         <div :class="[color.selectedClass, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
           <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
-          <span aria-hidden="true" :class="[color.class, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
+          <span aria-hidden="true" :class="[color.class, 'h-8 w-8 rounded-full border border-black border-opacity-10']" @click="changeColorHandler(color.image)" />
         </div>
       </RadioGroupOption>
     </div>
@@ -16,14 +16,21 @@
 <script setup>
 import { ref } from 'vue';
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
+import { changeColor } from '@/composables/useColor';
+
 const colors = [
-    { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-    { name: 'Sky Blue', class: 'bg-sky-300', selectedClass: 'ring-gray-400' },
-    { name: 'Blue', class: 'bg-blue-500', selectedClass: 'ring-gray-400' },
-    { name: 'Green', class: 'bg-green-400', selectedClass: 'ring-gray-400' },
-    { name: 'Red', class: 'bg-red-600', selectedClass: 'ring-gray-400' },
-    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    { name: 'White', class: 'bg-white', image: 'fashion_shirt1_white.png', selectedClass: 'ring-gray-400' },
+    { name: 'Sky Blue', class: 'bg-sky-300', image: 'fashion_shirt3_skyblue.png', selectedClass: 'ring-gray-400' },
+    { name: 'Blue', class: 'bg-blue-500', image: 'fashion_shirt4_blue.png', selectedClass: 'ring-gray-400' },
+    { name: 'Green', class: 'bg-green-400', image: 'fashion_shirt2_green.png', selectedClass: 'ring-gray-400' },
+    { name: 'Red', class: 'bg-red-600', image: 'fashion_shirt6_red.png',  selectedClass: 'ring-gray-400' },
+    { name: 'Black', class: 'bg-gray-900', image: 'fashion_shirt5_black.png',  selectedClass: 'ring-gray-900' },
   ]
   
 const selectedColor = ref(colors[0])
+
+const changeColorHandler = (color) => {
+    changeColor(color);
+};
+
 </script>
--- client/src/components/products/ProductImage.vue
+++ client/src/components/products/ProductImage.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="hidden xl:w-2/6 lg:w-2/5 w-80 md:block">
-    <img class="w-full" alt="image of a girl posing" :src="Image" />
+    <img class="w-full" alt="image of a shirt" :src="getImageUrl(Image)" />
   </div> 
   <div class="md:hidden">
-    <img class="w-full" alt="image of a girl posing" :src="Image" />
+    <img class="w-full" alt="image of a girl posing" :src="getImageUrl(Image)" />
   </div>
 </template>
 
 <script setup>
-import Image from '@/assets/images/products/fashion_shirt3_skyblue.png'
-
+//import ImageTest from '@/assets/images/products/' + Image
+import { Image }  from '@/composables/useColor';
+function getImageUrl(name) {
+  return require(`@/assets/images/products/${name}`);
+}
 </script>
