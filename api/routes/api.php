704c63dd5d2a7bd74f553e3aed52feead54d764a
Viviam <vivian.lopesg@gmail.com>
2024-07-25 17:01:57 +0100

    added api routes

---
+++ api/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::apiResource('products', 'ProductController');
+});
